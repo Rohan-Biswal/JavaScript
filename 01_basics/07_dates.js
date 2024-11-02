1176e3e652ecc90dcdf41ea2ffb5ed73d6c4a5c9
Rohan Biswal <110385146+Rohan-Biswal@users.noreply.github.com>
2024-11-02 08:33:56 +0000

    dates in js

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+let myDate1=new Date()
+console.log(myDate1);
+console.log(myDate1.toDateString());
+console.log(myDate1.toLocaleString());
+console.log(myDate1.toTimeString());
+let myDate2= new Date(2003,2,25)
+console.log(myDate2.toDateString());
+console.log(Date.now());
+
+
