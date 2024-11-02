3017541aa55d5f028b3dbdc66611e3b9b0f5ca70
Rohan Biswal <110385146+Rohan-Biswal@users.noreply.github.com>
2024-11-02 06:03:28 +0000

    comaprision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(5>3);
+// console.log(5>=3);
+// console.log(5<3);
+// console.log(5==5);
+// console.log(5<=5);
+
+
+// console.log("2">1);
+// console.log("02">1);//both will show true because it will first convert string to a number and then compare.
+
+// console.log(null>0);
+// console.log(null==0);//but here it will show false because == operator works differently then <= or>=.
+// console.log(null>=0);//this will show true because it treate null as zero and compare.
+
+
+// strict check === it will also check for the datatype.
+console.log("2"==2); //it will show true.
+console.log("2"===2);//here datatype is not same so it will show false.
+
+
