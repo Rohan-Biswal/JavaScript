0a050427c55126ff0eee92229f2fd6b16f64d0cb
Rohan Biswal <110385146+Rohan-Biswal@users.noreply.github.com>
2024-11-02 08:14:36 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+let score=100
+// console.log(score);
+let count=new Number(200)
+// console.log(count);
+// console.log(count.toString());
+
+let score2=200
+// console.log(score2.toFixed(2));//it will show vlaue till 2 decimal placed.
+let otherval1=2543.3464
+// console.log(otherval1.toPrecision(5));
+const otherval2=10000000
+// console.log(otherval2.toLocaleString());//according to us 
+// console.log(otherval2.toLocaleString('en-IN'));//according to indian
+
+/**********************************************Maths****************************************************/
+// console.log(Math);
+// console.log(Math.abs(-4));//it will change the all neg to positive value.
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.2));//it will round of max value.
+// console.log(Math.floor(4.9));//it will round off min value.
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10)+1);
+
+let min=10
+let max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);//always greater than min value.
+
+
+
+
+
+
