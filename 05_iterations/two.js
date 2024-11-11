8b5c915e9e64d94e17b2885760c6b333cd45011f
Rohan Biswal <110385146+Rohan-Biswal@users.noreply.github.com>
2024-11-11 17:53:31 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+//while loop
+// let i=0
+// while(i<10){
+//     console.log(`the index is ${i}`);
+//     i++;
+// }
+
+// let arr1=["batman","spiderman","krish","shaktiman","superman"]
+// let i=0;
+// while(i<arr1.length){
+//     console.log(arr1[i]);
+//     i++;
+// }
+
+//do while loop
+//the major difference between while and do while loop is if condition get false in the begning of
+//  the loop the while loop while never exicute but the do while will exicute only one time.
+let score=11;
+do{
+    console.log(`the current score is ${score}`);
+    score++;
+}while(score<=10);
